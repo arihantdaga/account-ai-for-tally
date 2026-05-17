ceacaef75a20616a9a40facb2cbb48e44fbc7543
Arihant Daga <arihant.ekism@gmail.com>
2026-05-18 01:39:43 +0530

    fixes

---
+++ code/vitest.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitest/config';
+export default defineConfig({
+    test: {
+        include: ['src/**/*.test.mts'],
+    },
+});
+//# sourceMappingURL=vitest.config.mjs.map
